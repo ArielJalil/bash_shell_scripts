1fc46d1f2c4e357495b9be9c9938671ce118a0cf
Ariel Jalil <ariel.jalil@datacom.co.nz>
2023-01-05 17:55:39 +1300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# bash_shell_scripts
+# Useful Bash shell scripts
+
+## tg_upgrade.sh
+
+Download Terragrunt binary file and replace/create symbolic links to the binary.
+
+This script will create a directory tree in your home directory to hold [Terragrunt](https://terragrunt.gruntwork.io/) binary file and also a symbolic link in `/usr/local/bin` to the latest version installed using this script.
+
+Running this script periodically will help to keep your Terragrunt binary file up to date.
+
+**Note:** keep in mind the script requires you to install WGET command before run it.
+
+## Author and Lincense:
+
+This script has been written by [Ariel Jall](https://github.com/ArielJalil) and it is released under [Apache License 2.0](https://www.apache.org/licenses/LICENSE-2.0).
---
+++ tg_upgrade.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+#
+# Download Terragrunt binary file and replace/create symbolic links to the binary.
+#
+# Requisites:
+# - WGET command installed
+#
+# Arguments ore nly required if an specific Terragrunt version is required, otherwhise none are required to deploy latest version.
+#
+# $1 = Version
+# $2 = Release
+#
+# i.e. Pass specific Version and Release to deploy:
+# $ tg_upgrade.sh 42 5
+#
+# i.e. If no version and release are passed as arguments the script will download the latest available version:
+# $ tg_upgrade.sh
+#
+
+# Terragrunt install URL's:
+TG_RELEASES_URL="https://github.com/gruntwork-io/terragrunt/releases"
+TG_DOWNLOAD_URL="${TG_RELEASES_URL}/download"
+
+# Evaluate arguments: 
+if [ $# -eq 0 ]
+then
+	# Look for the latest Terragrunt version_release available online:
+	TG_LATEST=$(wget ${TG_RELEASES_URL} -q -O - | grep " v0." | head -1 | awk '{print $1}')
+else
+	if [ $# -eq 2 ]
+	then
+		TG_LATEST="v0.${1}.${2}"   # i.e. v0.42.5
+	else
+		echo "This script works with none or exactly 2 arguments in the following order: Version Release"
+		echo -e "i.e. ${0} 42 5\n"
+		exit 
+	fi
+fi
+
+# Set variables with the files and directories names:
+BASE_DIR="${HOME}/bin/terragrunt"
+NEW_DIR="${BASE_DIR}/${TG_LATEST}"
+SYM_LINK="${BASE_DIR}/latest"
+BIN_LINK="/usr/local/bin/terragrunt"
+TG_BINARY="terragrunt_darwin_amd64"  # MAC
+# TG_BINARY="terragrunt_linux_amd64" # Linux
+
+# Create Base directory if it doesn't exist:
+if [ ! -d "${BASE_DIR}" ]
+then
+	echo "Creating base directory to hold Terragrunt binary."
+	mkdir -p ${BASE_DIR}
+fi
+
+# Check if the requested version exist and create the new directory if it doesn't:
+if [ -d "${NEW_DIR}" ]
+then
+	echo -e "Terragrunt ${TG_LATEST} is already installed, No action taken.\n"
+	exit
+else
+	mkdir ${NEW_DIR}
+	cd ${NEW_DIR}
+fi
+#
+# Install the new Terragrunt version:
+# 
+echo "Downloading Terragrunt ${TG_LATEST} binary."
+wget ${TG_DOWNLOAD_URL}/${TG_LATEST}/${TG_BINARY} -q
+
+if [ $? -ne 0 ]
+then
+	echo "Terragrunt binary file download FAILED."
+	cd -
+	rm -rf ${NEW_DIR}
+	exit
+fi
+
+chmod +x ${TG_BINARY}
+
+if [ -f "${SYM_LINK}" ]
+then
+	echo "Deleting old symbolic link."
+	rm ${SYM_LINK}
+fi
+
+# Create the new symbolic link to the latest terragrunt binary:
+ln -s ${NEW_DIR}/${TG_BINARY} ${SYM_LINK}
+
+if [ ! -f ${BIN_LINK} ]
+then
+	echo "Creating new terragrunt symbolic link at /usr/local/bin directory if it doesn't exist yet."
+	sudo ln -s ${SYM_LINK} ${BIN_LINK}
+fi
+
+cd -
+echo -e "\nNew version has been deployed.\n"
+
